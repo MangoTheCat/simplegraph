96aac565ffae185107321c9454ea7988aa69a19b
Gabor Csardi <csardi.gabor@gmail.com>
2015-11-28 11:12:09 +0000

    Initial commit of Mason template

---
+++ R/package.R
@@ -0,0 +1,8 @@
+
+#' Simple Graph Data Types and Basic Algorithms
+#'
+#' TODO
+#'
+#' @docType package
+#' @name simplegraph
+NULL
---
+++ tests/testthat/test.R
@@ -0,0 +1,8 @@
+
+context("simplegraph")
+
+test_that("simplegraph works", {
+
+  expect_true(TRUE)
+
+})
